dce53e43862a2a98af3b56272cc588652e3d60ec
Ghayath Alali <ghayathAlali2003@gmail.com>
2026-03-07 20:00:27 +0300

    Maintain a shopping cart where items can be addded removed or replaced and notify the ui

--- ObservableCollectionlProblems04/ObservableCollectionlProblemsProgram.cs
+++ ObservableCollectionlProblems04/ObservableCollectionlProblemsProgram.cs
@@ -12,8 +12,12 @@ namespace ObservableCollectionlProblems04
     {
         public static void Main(string[] agrs)
         {
+            // Problem 02
+            ShoppinngProcess();
+
+
             // Problem 01
-            ClassRoom();
+            // ClassRoom();
         }
 
         // Maintain and display a dynamic list of students in a classroom
@@ -121,8 +125,83 @@ namespace ObservableCollectionlProblems04
             }
         }
 
-     
+
         // Maintain a shopping cart where items can be addded removed or replaced and notify the ui
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Catigory { get; set; }
+            public Product(int id, string name, string catigory)
+            {
+                Id = id;
+                Name = name;
+                Catigory = catigory;
+            }
+
+            public override string ToString()
+            {
+                return $"{this.Id} | {this.Name} | {this.Catigory}";
+            }
+        }
+
+        public static void ShoppinngProcess()
+        {
+            Product product1 = new Product(1, "Laptop", "Elictric");
+            Product product2 = new Product(2, "Mobile", "Elictric");
+            Product product3 = new Product(3, "Chary", "Frute");
+            Product product4 = new Product(4, "Eggs", "Food");
+            Product product5 = new Product(5, "Tomato", "Food");
+            Product product6 = new Product(6, "Bods", "Elictric");
+            ObservableCollection<Product> Cart = new ObservableCollection<Product>();
+            Cart.CollectionChanged += Cart_CollectionChanged;
+
+            Cart.Add(product1);
+            Cart.Add(product2);
+            Cart.Add(product3);
+            Cart.Add(product4);
+            Cart.Add(product5);
+            Cart.Remove(product1);
+            Cart.Move(2,3);
+            Cart[0]=product6;
+        }
+
+        private static void Cart_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch(e.Action)
+            {
+                case NotifyCollectionChangedAction.Add: 
+                    {
+                        Console.WriteLine("Prodect added to the cart");
+                        break;
+                    }
+
+                case NotifyCollectionChangedAction.Remove:
+                    {
+                        Console.WriteLine("Prodect removed from the cart");
+                        break;
+                    }
+
+                case NotifyCollectionChangedAction.Replace:
+                    {
+                        Console.WriteLine("Prodect replaced with another from the cart");
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Move:
+                    {
+                        Console.WriteLine("Prodect moved from the cart");
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Reset:
+                    {
+                        Console.WriteLine("The cart is empty");
+                        break;
+                    }
+
+            }
+        }
+
+
 
 
         // Display a list of real-time stock prices that update dynamically
