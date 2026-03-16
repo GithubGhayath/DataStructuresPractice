2f25b94dd529e074c0deeebd26eb85f4c18da3e9
Ghayath Alali <ghayathAlali2003@gmail.com>
2026-03-16 03:02:15 +0300

    Use a jagged array to store marks of students across different subjects

--- JaggedArrayProblems05/Program.cs
+++ JaggedArrayProblems05/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using Shared.Print;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Problem01
+        StudentMarks();
+    }
+
+    // Use a jagged array to store marks of students across different subjects.
+    public static void StudentMarks()
+    {
+        int[][] Marks = new int[3][];
+        Marks[0] = new int[3] {90,85,88 };
+        Marks[1] = new int[2] {90,85};
+        Marks[2] = new int[4] {90,85,88,70 };
+
+        Marks.Print("Student");
     }
 }
