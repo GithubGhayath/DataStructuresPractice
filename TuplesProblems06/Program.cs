b243904bf6df855752afaf0b3a47c83429d527c3
Ghayath Alali <ghayathAlali2003@gmail.com>
2026-03-23 02:12:31 +0300

    Create a function that returns a student's name, age, and grade

--- TuplesProblems06/Program.cs
+++ TuplesProblems06/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Problem 01
+        PrintStudentInfo();
+    }
+
+    // Create a function that returns a student's name, age, and grade.
+    public static (string, int, int) GetStudentInfo()
+    {
+        return ("Ghayath", 24, 13);
+    }
+    public static void PrintStudentInfo()
+    {
+        (string, int, int) StudentInfo = GetStudentInfo();
+        Console.WriteLine($"{StudentInfo.Item1} | {StudentInfo.Item2} | {StudentInfo.Item3}");
+    }
+}
