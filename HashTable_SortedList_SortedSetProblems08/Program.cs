e1d7b280b388f5110a11e251aba16d4e37c1decc
Ghayath Alali <ghayathAlali2003@gmail.com>
2026-03-27 14:35:45 +0300

    Write a program to copy all key-value pairs from one Hashtable to another

--- HashTable_SortedList_SortedSetProblems08/Program.cs
+++ HashTable_SortedList_SortedSetProblems08/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+﻿using Microsoft.VisualBasic;
+using Shared.Print;
+using System.Collections;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Problem 01
+        TestCopyFunction();
+    }
+
+    // Write a program to copy all key-value pairs from one Hashtable to another.
+    private static Hashtable CopyHashTable(Hashtable ht)
+    {
+        Hashtable hashtable = new Hashtable();
+        foreach (DictionaryEntry kvp in ht)
+        {
+            hashtable.Add(kvp.Key, kvp.Value);
+        }
+        return hashtable;
+    }
+
+    public static void TestCopyFunction()
+    {
+        Hashtable hashtable = new Hashtable();
+        hashtable.Add("a", "b");
+        hashtable.Add(23, 123);
+        hashtable.Add("a3", "dfa");
+
+
+        CopyHashTable(hashtable).Print("The copied hash table: ");
     }
 }
