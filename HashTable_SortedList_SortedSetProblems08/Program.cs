4b8cba267ded0262b51de5fca8ef5b222a1d3da5
Ghayath Alali <ghayathAlali2003@gmail.com>
2026-04-01 11:55:08 +0300

    Given a list with duplicate values, use a SortedSet to remove duplicates and sort it

--- HashTable_SortedList_SortedSetProblems08/Program.cs
+++ HashTable_SortedList_SortedSetProblems08/Program.cs
@@ -6,8 +6,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        // Problem 02
+        RemoveDubplicate();
+
         // Problem 01
-        TestCopyFunction();
+        // TestCopyFunction();
     }
 
     // Write a program to copy all key-value pairs from one Hashtable to another.
@@ -31,4 +34,16 @@ internal class Program
 
         CopyHashTable(hashtable).Print("The copied hash table: ");
     }
+
+    // Given a list with duplicate values, use a SortedSet to remove duplicates and sort it.
+    public static void RemoveDubplicate()
+    {
+        List<int> Numbers = new List<int> { 1, 1, 1, 1, 2, 3, 4, 3, 3, 4, 5, 5, 3, 2, 1, 6, 7, 5, 5, 4, 4, 3, 4, 5, 6, 7, 7, 8, 8, 9, 9, 9, 0, 10, 6, 7, 8, 9, 5, 4 };
+
+        SortedSet<int> Number1 = new SortedSet<int>(Numbers);
+        Numbers.Print("List With duplicate: ");
+        Number1.Print("\n\nList without duplicate:");
+    }
+
+
 }
